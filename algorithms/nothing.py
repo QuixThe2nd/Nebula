b14ef011d5ab788343b05b66dc329ea3cfed8088
Auxority <gide0013@hz.nl>
2023-09-29 19:34:47 +0200

    add empty algo example

---
+++ algorithms/nothing.py
@@ -0,0 +1,9 @@
+class Nothing():
+    def __init__(self):
+        pass
+
+    def compress(image: list[list[int]]) -> str:
+        pass
+
+    def decompress(compressed: str) -> list[list[int]]:
+        pass
--- image.py
+++ image.py
@@ -4,5 +4,5 @@ class Image:
     def __init__(self, path: str):
         self.path = path
 
-    def read(self):
+    def read(self) -> list[list[int]]:
         return cv2.imread(self.path, cv2.IMREAD_GRAYSCALE)
