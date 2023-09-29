8300e8cb6157ebc2e7430bd64a4fc4f95d927cc4
Parsa Yazdani <3parsa3@gmail.com>
2023-09-30 04:53:28 +1000

    Fix serialiser header

--- modules/serializer.py
+++ modules/serializer.py
@@ -6,7 +6,7 @@ class Serializer:
         pass
 
     def serialize(self, data: list[list[int]]) -> bytes:
-        output = b'\x00\x00' +  b''.join([struct.pack('H', number) for number in dimensions(data)])
+        output = b'\x00\x00' +  b''.join([number.to_bytes(2, byteorder='big') for number in dimensions(data)])
         for row in data:
             for pixel in row:
                 output += struct.pack('H', pixel)
