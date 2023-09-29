4d2413b74fba32d9ee3ac6105ab71e9e6ba15901
Auxority <gide0013@hz.nl>
2023-09-29 20:50:53 +0200

    only 0 or 1 values

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from modules.serializer import Serializer
 IMAGE_PATH = "test.png"
 image = Image(IMAGE_PATH)
 data = image.read()
+print(f"Image data: {data}")
 
 serializer = Serializer()
 serialized = serializer.serialize(data)
--- modules/image.py
+++ modules/image.py
@@ -1,8 +1,11 @@
 import cv2
+import numpy as np
 
 class Image:
     def __init__(self, path: str):
         self._path: str = path
 
     def read(self) -> list[list[int]]:
-        return cv2.imread(self._path, cv2.IMREAD_GRAYSCALE).tolist()
+        pixels = cv2.imread(self._path, cv2.IMREAD_GRAYSCALE)
+        pixels = np.divide(pixels, 255)
+        return (pixels > 0.5).astype(int).tolist()
