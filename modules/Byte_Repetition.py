a70603e8df29247b3729f0795185b0a477f4a88d
Auxority <gide0013@hz.nl>
2023-09-30 21:47:46 +0200

    fix types

--- modules/Byte_Repetition.py
+++ modules/Byte_Repetition.py
@@ -6,17 +6,16 @@ class Byte_Repetition:
         self.dimensions = dimensions
         self.id = 1
 
-    def input(self, data: list[list[int]]) -> bytes:
+    def input(self, data: bytes) -> bytes:
         output = int(self.id).to_bytes(2, byteorder='big')
-        last_byte = None
+        last_byte = data[0]  # Initialize last_byte to the first byte of the input data
         repetitions = 0
         for byte in data:
             if byte == last_byte:
                 repetitions += 1
             else:
-                if last_byte != None:
-                    output += repetitions.to_bytes(2, byteorder='big') + last_byte.to_bytes()
+                output += repetitions.to_bytes(2, byteorder='big') + last_byte.to_bytes(1, byteorder='big')
                 last_byte = byte
                 repetitions = 1
-        output += repetitions.to_bytes(2, byteorder='big') + last_byte.to_bytes()
+        output += repetitions.to_bytes(2, byteorder='big') + last_byte.to_bytes(1, byteorder='big')
         return output
